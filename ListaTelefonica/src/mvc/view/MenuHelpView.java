31b36b0c6ff278e9df9f2bee6d264a85414994bc
Fabricio Ladeira <fabriciofjl@gmail.com>
2014-03-15 16:03:12 -0300

    Visão do help da app

---
+++ ListaTelefonica/src/mvc/view/MenuHelpView.java
@@ -0,0 +1,16 @@
+package mvc.view;
+
+public class MenuHelpView {
+	
+	public void Exibir()
+	{
+		System.out.println("INCLUIR NOME TELEFONE");
+		System.out.println("LISTAR");						
+		System.out.println("Sair");			
+	}
+
+}
+
+
+	
+

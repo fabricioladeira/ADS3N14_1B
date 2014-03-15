1795581bb96c58312e3767297800a55f5d9fbc8c
Fabricio Ladeira <fabriciofjl@gmail.com>
2014-03-15 14:24:03 -0300

    Hello World

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,13 @@
+import static java.util.*;
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("Hello World";)
+	}
+
+}

c1c0cb0881a9f8e973e39b517939ca88b3147c15
Fabricio Ladeira <fabriciofjl@gmail.com>
2014-04-12 09:43:22 -0300

    Começando a lógica para distribuir os navios nos tabuleiros

--- BatalhaNaval/src/Batalha.java
+++ BatalhaNaval/src/Batalha.java
@@ -1,11 +1,14 @@
 import static java.lang.System.out;
 
+import java.util.Random;
+
 public class Batalha {
 
 	public static char[] cabecalho = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
 			'I', 'J' };
 	public static int pontos = 15;
 	public static String[][] mapa = new String[10][10];
+	public static Navio[][] navios = new Navio[5][5];
 
 	/**
 	 * @param args
@@ -15,8 +18,62 @@ public class Batalha {
 
 		out.println("Batalha Naval");
 
+		InicializaNavios(1);
 		MontaMapa();
 
+	}
+
+	/**
+	 * @param numeroTotalNavios
+	 */
+	private static void InicializaNavios(int numeroTotalNavios) {
+
+		Random sorteio = new Random();
+		for (int i = 0; i < numeroTotalNavios; i++) {
+			
+//			Sorteia o tipo do Navio a ser usado
+//			1. 1 “porta-aviões” com 5 unidades de tamanho.!
+//			2. 2 “destroyers” com 4 unidades de tamanho.!
+//			3. 2 “fragatas” com 3 unidades de tamanho.!
+//			4. 3 “torpedeiros” com 2 unidades de tamanho.!
+//			5. 5 submarinos, com 1 unidade de tamanho.!
+			int t = sorteio.nextInt(5); 
+						
+			// Sorteia a posição x inicial do navio
+			int x = 3; //sorteio.nextInt(10); 
+			
+			// Sorteia a posição y inicial do navio
+			int y = 2; //sorteio.nextInt(10); 
+			
+			//Sorteia a direção que o navio vai fica 1=Vertical ou 2 = Horizontal
+			int direcao = 1;//sorteio.nextInt(2);
+			
+			//Cria Navio
+			Navio nav = new Navio(t,x,y);
+			
+			//Verifica direcao = Vertical
+			if(direcao == 1){
+				
+				int totalCasasPreencher = nav.getTamanho() + y; 
+				while (totalCasasPreencher > mapa.length)
+				{
+					y -=1;
+					totalCasasPreencher = nav.getTamanho() + y; 					
+				}
+				
+				//Atualiza posição inicial
+				nav.setY(y);
+				
+				//Coloca navio na posição dele.
+				navios[x][y] = nav;
+			}
+			
+			
+			
+		}
+		
+		
+		
 	}
 
 	private static void MontaMapa() {
@@ -32,11 +89,12 @@ public class Batalha {
 		for (int l = 0; l < mapa.length; l++) {
 
 			// out.print(l + "  ");
-
 			linha = l + " ";
 
 			for (int c = 0; c < mapa.length; c++) {
 
+				GetNavio(l,c);
+				
 				linha += " .  ";
 
 			}
@@ -45,6 +103,14 @@ public class Batalha {
 
 		}
 
+	}
+	
+	// Verifica se tem um navio na posição e retorna ele.
+	private Navio GetNavio(int l, int c)
+	{
+		
+		
+	}
 	}
 
 }
--- BatalhaNaval/src/Navio.java
+++ BatalhaNaval/src/Navio.java
@@ -1,9 +1,35 @@
 
 public class Navio {
 	
-	private int id;
+	// Tipo do Navio
+	private int id; 
+	
+	//Tamanho do Navio
 	private int tamanho;
+	
+	//Posicao Inicial X
+	private int X;
+	
+	//Posicao inical Y;
+	private int Y;
 
+	
+	public int getX() {
+		return X;
+	}
+
+	public void setX(int x) {
+		X = x;
+	}
+
+	public int getY() {
+		return Y;
+	}
+
+	public void setY(int y) {
+		Y = y;
+	}
+	
 	public int getTamanho() {
 		return tamanho;
 	}
@@ -20,10 +46,35 @@ public class Navio {
 		this.id = id;
 	}
 	
-	public Navio(int id, int tamanho)
+	public Navio(int id, int x, int y)
 	{
-		this.setId(id);
-		this.setTamanho(tamanho);		
+		switch (id) {
+		case 1:
+			this.setTamanho(5);		
+			break;
+		
+		case 2:
+			this.setTamanho(4);		
+			break;
+			
+		case 3:
+			this.setTamanho(3);		
+			break;
+			
+		case 4:
+			this.setTamanho(2);		
+			break;
+			
+		case 5:
+			this.setTamanho(1);		
+			break;
+
+		default:
+			break;
+		}
+		
 	}
+
+
 
 }
